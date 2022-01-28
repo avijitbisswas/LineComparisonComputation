c7bb35ae9020cd4bd57f571c3dbdb5bdccfc6e12
avijit <avijit02biswas@gmail.com>
2022-01-28 23:37:32 +0530

    [Avijit] linelength

---
+++ LineComparisonComputation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonComputation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Comptation Program");
+            Console.ReadLine();
+          
+            LineLength linelength = new LineLength();
+            linelength.ComputeLines();
+            Console.ReadLine(); 
+           
+
+        }
+    }
+}
+    
+
+            
+            
+
+           
+
+
+    
